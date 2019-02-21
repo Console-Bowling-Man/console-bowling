bf003547c9f998d79057e686249595a1540565d1
Jesse Hufstetler <Jesse.Hufstetler@gmail.com>
2019-02-21 18:36:11 -0500

    Sillyness

--- ConsoleBowling/Program.cs
+++ ConsoleBowling/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Console = System.Console;
 
 namespace ConsoleBowling
 {
@@ -10,6 +12,38 @@ namespace ConsoleBowling
     {
         static void Main(string[] args)
         {
+            var oldBackground = Console.BackgroundColor;
+            var oldForeground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(" ██████╗ ██████╗ ███╗   ██╗███████╗ ██████╗ ██╗     ███████╗");
+            Console.WriteLine("██╔════╝██╔═══██╗████╗  ██║██╔════╝██╔═══██╗██║     ██╔════╝");
+            Console.WriteLine("██║     ██║   ██║██╔██╗ ██║███████╗██║   ██║██║     █████╗  ");
+            Console.WriteLine("██║     ██║   ██║██║╚██╗██║╚════██║██║   ██║██║     ██╔══╝  ");
+            Console.WriteLine("╚██████╗╚██████╔╝██║ ╚████║███████║╚██████╔╝███████╗███████╗");
+            Console.WriteLine(" ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚══════╝╚══════╝");
+            Console.WriteLine("                                                            ");
+            Console.WriteLine("     ██████╗  ██████╗ ██╗    ██╗██╗     ███████╗██████╗     ");
+            Console.WriteLine("     ██╔══██╗██╔═══██╗██║    ██║██║     ██╔════╝██╔══██╗    ");
+            Console.WriteLine("     ██████╔╝██║   ██║██║ █╗ ██║██║     █████╗  ██████╔╝    ");
+            Console.WriteLine("     ██╔══██╗██║   ██║██║███╗██║██║     ██╔══╝  ██╔══██╗    ");
+            Console.WriteLine("     ██████╔╝╚██████╔╝╚███╔███╔╝███████╗███████╗██║  ██║    ");
+            Console.WriteLine("     ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝  ╚═╝    ");
+            Console.WriteLine("                                                            ");
+            Console.WriteLine("                ██████╗  ██████╗  ██████╗                   ");
+            Console.WriteLine("                ╚════██╗██╔═████╗██╔═████╗                  ");
+            Console.WriteLine("                 █████╔╝██║██╔██║██║██╔██║                  ");
+            Console.WriteLine("                 ╚═══██╗████╔╝██║████╔╝██║                  ");
+            Console.WriteLine("                ██████╔╝╚██████╔╝╚██████╔╝                  ");
+            Console.WriteLine("                ╚═════╝  ╚═════╝  ╚═════╝                   ");
+            Console.BackgroundColor = oldBackground;
+            Console.ForegroundColor = oldForeground;
+            Console.WriteLine();
+            Console.WriteLine(">>>>>        Do you have what it takes? (TM)           <<<<<");
+            Console.WriteLine();
+            Console.WriteLine("Press the any key to begin...");
+            Console.ReadKey();
+
         }
     }
 }
